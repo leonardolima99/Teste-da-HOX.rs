f473bbcac95a91d0b2579fba89fd940299cd101e
Leonardo Lima <dev.leonardolima@gmail.com>
2022-05-27 10:29:26 -0400

    add and config redux-persist

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,9 +4,15 @@ import App from "./App";
 import "./index.css";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -1,11 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
 
 import counterReducer from "./reducers/counterSlice";
 
+const reducers = combineReducers({
+  counter: counterReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
