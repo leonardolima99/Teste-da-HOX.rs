fbb0dbd1f01b46b69e581de8e6f2b861f588503d
Leonardo Lima <dev.leonardolima@gmail.com>
2022-06-02 16:02:53 -0400

    enable host for expose url to use

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
   },
 });
